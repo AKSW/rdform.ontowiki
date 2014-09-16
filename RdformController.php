cbffefbd33665175743d2aa865031d7933138d6b
Simeon Ackerman <amseon@web.de>
2014-09-16 14:20:42 +0200

    add submodule rdform

--- RdformController.php
+++ RdformController.php
@@ -20,8 +20,13 @@ class RdformController extends OntoWiki_Controller_Component
     public function indexAction()
     {
         $owApp = OntoWiki::getInstance();
+        
+        $this->view->headLink()->appendStylesheet( $owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/css/bootstrap.min.css' );
+        $this->view->headLink()->appendStylesheet( $owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/css/rdform.css' );
+
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/jquery.jsonrpcclient.js/jquery.jsonrpcclient.js');
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/jsonld.js/js/jsonld.js');
+        $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/js/rdform.js');
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/jsonread.js');
 
         $this->view->selectedModel = $owApp->selectedModel;
