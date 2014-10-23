ab29a096524964063bb325f53c3e2f9edaf1d7be
Simeon Ackerman <amseon@web.de>
2014-10-23 14:56:29 +0200

    Add jqueryui files

--- RdformController.php
+++ RdformController.php
@@ -29,8 +29,10 @@ class RdformController extends OntoWiki_Controller_Component
         
         //$this->view->headLink()->appendStylesheet( $owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/css/bootstrap.min.css' );
         $this->view->headLink()->appendStylesheet( $owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/css/rdform.css' );
+        //$this->view->headLink()->appendStylesheet( $owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/css/css/smoothness/jquery-ui.custom.min.css' );
 
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/jquery.jsonrpcclient.js/jquery.jsonrpcclient.js');
+        //$this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/js/jquery-ui.custom.min.js');
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/jsonld.js/js/jsonld.js');
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/lib/rdform/js/rdform.js');
         $this->view->headScript()->appendFile($owApp->extensionManager->getComponentUrl('rdform') . 'js/jsonread.js');
