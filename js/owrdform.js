47caf1bac871b9755ea35feb0fe3de42ddba557b
Simeon Ackerman <amseon@web.de>
2014-11-07 14:24:03 +0100

    Get template from self.template. Save resourceId in .data

--- js/owrdform.js
+++ js/owrdform.js
@@ -2,6 +2,7 @@ function OntoWikiRDForm (){
 	var self = this;	
 	self.$container = $(".active-tab-content"); /// the container element for the form. In OntoWiki by default the content of the active tab
 	self.$elem = null; // the form element
+	self.template = "default";
 	var rdformId = null;
 
 	// public function to init a new form. Callback returns the submitted result of the form
@@ -12,10 +13,10 @@ function OntoWikiRDForm (){
 		if ( ! self.$elem ) {
 			self.$elem = $('<form id="'+rdformId+'" class="rdform form-horizontal" onsubmit="return false;"></form>');	
 		}
-
-		self.$elem.append('<input type="hidden" id="resourceIri" value="'+resourceIri+'" />');
+		self.$elem.data("resourceIri", resourceIri);
+		/*self.$elem.append('<input type="hidden" id="resourceIri" value="'+resourceIri+'" />');
 		self.$elem.append('<input type="hidden" id="dataHash" value="'+data.dataHash+'" />');
-		self.$elem.append('<input type="hidden" id="editable" value="'+data.editable+'" />');
+		self.$elem.append('<input type="hidden" id="editable" value="'+data.editable+'" />');*/
 
 		self.$container.append(self.$elem);
 
@@ -28,10 +29,15 @@ function OntoWikiRDForm (){
 
     // private function creates the form.
     function newForm( data, callback) {
+    	if ( data.length > 0 ) {
+    		var dtype = data[0]["@type"][0];
+    		self.template = dtype.split('/').reverse()[0];
+    	}
     	self.$elem.RDForm({
-            template: urlBase + "extensions/rdform/public/form_pfarrerbuch.html",
+            template: urlBase + "extensions/rdform/public/form_pfarrerbuch-"+self.template+".html",
             hooks: urlBase + "extensions/rdform/public/hooks_pfarrerbuch.js",
             //lang: parent.urlBase + "extensions/rdform/public/lang",
+            //debug: true,
             data: data,
 
             submit: function() {
